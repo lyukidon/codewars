8bca767e472cad5d4992af7d3931fd7fb438c106
lyukidon <64675543+lyukidon@users.noreply.github.com>
2022-12-09 14:32:45 +0900

    Campfire building

---
+++ 6kyu/Campfire_building.js
@@ -0,0 +1,15 @@
+function isConstructable(a){
+  if (Math.sqrt(a)===parseInt(Math.sqrt(a))){
+    return true;
+  }else{
+    let i=1;
+    while(a>Math.pow(i,2)){
+      let squared=Math.pow(i,2)
+      if (Math.sqrt(a-squared) === parseInt(Math.sqrt(a-squared))){
+        return true;
+      }
+      i++;
+    }
+    return false;
+  }
+}
