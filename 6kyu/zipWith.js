153015592787abb6d22204411a293509364676a2
lyukidon <64675543+lyukidon@users.noreply.github.com>
2023-01-28 01:29:19 +0900

    zipWith

--- 6kyu/zipWith.js
+++ 6kyu/zipWith.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/5825792ada030e9601000782/solutions/javascript
+
+function zipWith(fn, a0, a1) {
+    return a0.reduce((save, cur, idx) => {
+        if (cur !== undefined && a1[idx] !== undefined) {
+            save.push(fn(cur, a1[idx]));
+        }
+        return save;
+    }, []);
+}
